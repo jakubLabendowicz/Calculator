8f41ebdeeb80b93977eb16b38fde87228e90ab10
jalabendowicz <jalabendowicz@gmail.com>
2023-10-10 14:45:54 +0200

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by student on 10/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
